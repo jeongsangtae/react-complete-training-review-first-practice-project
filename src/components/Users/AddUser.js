a450441a460b6592bb651bd8c230fc78d2c18509
jeongsangtae <wjdtkdxo55@naver.com>
2023-08-29 01:01:54 +0900

    React Complete Training Review First Practice Project - index.css backgroundColor add + Button component button type edit

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
-import styles from "./AddUser.module.css";
 import ErrorModal from "../UI/ErrorModal";
+
+import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const [userName, setUserName] = useState("");
